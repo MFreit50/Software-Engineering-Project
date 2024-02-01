f1b5506f44ec6853da90bf7278793dde62b312aa
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-02-01 16:17:34 -0500

    Add files via upload

---
+++ DataEngineAPI.java
@@ -0,0 +1,8 @@
+public interface DataEngineAPI {
+    // Method for reading data from the input source
+    void readData(InputSource inputSource);
+
+    // Method for writing data to the output destination
+    void writeData(OutputDestination outputDestination, String data);
+
+}
