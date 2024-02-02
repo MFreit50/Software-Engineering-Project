aea57a31f36be93339d6c4f4dfd06c96a7e61f35
MFreit50 <93632207+MFreit50@users.noreply.github.com>
2024-02-02 14:55:54 -0500

    added features

---
+++ ComputeEngineAPI.java
@@ -0,0 +1,13 @@
+public interface ComputeEngineAPI{
+	// Initialize process
+	void initialize(Key key);
+
+	// Perform computation
+	void computeProcess();
+
+	// Read data from the input source
+	void readData(InputSource inputSource);
+
+    // Write data to the output destination
+    void writeData(OutputDestination outputDestination, String data);
+}
---
+++ Prototype.java
@@ -0,0 +1,29 @@
+public class Prototype {
+    public void testDataEngineAPI(DataEngineAPI engine) {
+        // Read data from the input source
+        engine.readData(InputSource.LOCAL_FILE);
+
+        // Write data to the output destination
+        engine.writeData(OutputDestination.DATABASE, "Processed data");
+    }
+
+    //Prototype
+    public void testUserEngineAPI(UserEngineAPI engine) {
+        // Set input source to a local file
+        engine.setInputSource(InputSource.LOCALFILE);
+
+        // Set output destination to a database
+        engine.setOutputDestination(OutputDestination.DATABASE);
+
+        // Set delimiter for output to semicolon
+        engine.setDelimiter(";");
+
+        //Set default delimiters
+        engine.setDefaultDelimiters();
+        
+        //compute and get return
+    }
+
+}
+
+//Prototype
