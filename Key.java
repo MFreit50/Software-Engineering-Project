c93fecd7728391f9d7e23a4a327b2e3018d13130
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-02-08 16:58:18 -0500

    Add files via upload
    Added required interfaces for the existing APIs

---
+++ Key.java
@@ -0,0 +1,16 @@
+public class Key{
+    private String key;
+
+    public Key(String key){
+        this.key = key;
+    }
+
+    public String getKey(){
+        return key;
+    }
+
+    public void setKey(String key){
+        this.key = key;
+    }
+	
+}
