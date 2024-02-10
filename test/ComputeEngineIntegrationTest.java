c4c82d234af80f68bb649bacf662679dcf22d0c0
MFreit50 <93632207+MFreit50@users.noreply.github.com>
2024-02-10 17:51:19 -0500

    Create ComputeEngineIntegrationTest.java

---
+++ test/ComputeEngineIntegrationTest.java
@@ -0,0 +1,8 @@
+public class ComputeEngineIntegrationTest{
+   public ComputeEngine computeEngine = new ComputeEngine();
+   public TestDataEngineAPI dataEngine = new TestDataEngineAPI();
+   public UserEngineAPI userEngine = new UserEngineAPI();
+
+   dataEngine.readData(10,2,3);
+
+}
