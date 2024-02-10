e4923138ed87e8d6a694aecc224f872497065c66
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-02-10 17:21:24 -0500

    Add files via upload
    Added in-memory implementation for Input source

---
+++ inMemoryImpInputSource.java
@@ -0,0 +1,8 @@
+import java.util.List;
+import java.util.ArrayList;
+public class inMemoryImpInputSource implements InputSource {
+    private List <Integer> data = new ArrayList<>();
+    public List<Integer> getData() {
+        return data;
+    }
+}
