d3cf574fd8c15585511f5e5105dbbaf7d146adda
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-02-17 20:45:20 -0500

    Updated UserEngine API
    added getter methods and removed key generator method as it is not needed

--- src/UserEngine.java
+++ src/UserEngine.java
@@ -4,24 +4,29 @@ public class UserEngine implements UserEngineAPI{
     private String outputDestination;
     private Character delimiter;
 
+    //Mutator methods
     public void setInputSource(String inputSource){
         this.inputSource = inputSource;
     }
-
     public void setOutputDestination(String outputDestination){
         this.outputDestination = outputDestination;
     }
-
     public void setDelimiter(Character delimiter) {
         this.delimiter = delimiter;
     }
-
     public void setDefaultDelimiter() {
         this.delimiter = ';';
     }
 
-    public String generateKey() {
-        return "";
+    //Accessor methods
+    public String getInputSource(){
+        return inputSource;
+    }
+    public String getOutputDestination(){
+        return outputDestination;
+    }
+    public Character getDelimiter(){
+        return delimiter;
     }
 
     public JobStatus.JobStatusCheck checkJobStatus() {
