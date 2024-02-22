1ac06d360a4d958581bd8675632d600b54aa6b04
MFreit50 <93632207+MFreit50@users.noreply.github.com>
2024-02-22 17:28:05 -0500

    Factorial implementation w/ fixed output (#72)
    * Factorial implementation fixed
    
    Factorial computation w/ proper output delimiter and format
    
    * Update FactoringImp.java (#70)
    
    - removed string variable
    
    ---------
    
    Co-authored-by: TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>

---
+++ FactoringImp.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.List;
+public class FactoringImp {
+    private String delimiter = ";";
+    public List<String> findFactors(int [] nums){
+        List<String> factors = new LinkedList<>();
+        for(int i : nums) {
+            factors.add(findFactorsHelper(i));
+            factors.add(delimiter);
+        }
+        return factors;
+    }
+    private String findFactorsHelper(int num) {
+        StringBuilder factorString = new StringBuilder();
+        factorString.append(num).append("=");
+        for (int i = 1; i<=num; ++i){
+            if(num % i == 0) {
+                factorString.append(i).append(",");
+            }
+        }
+        factorString.deleteCharAt(factorString.length() - 1).append(delimiter);
+        return factorString.toString();
+    }
+    public  void setDelimiter(String delim) {
+        delimiter = delim;
+    }
+}
+
