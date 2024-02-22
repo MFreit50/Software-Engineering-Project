61e85c61618232e0e9e9537d28b65c929d231c83
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-02-22 16:40:29 -0500

    Updated InMemoryImpOutputConfig (#63)
    * Updated InMemoryImpOutputConfig
    
    Updated InMemoryImpOutputConfig for new Output Config
    
    * Update InMemoryImpOutputConfig.java
    
    - changed name of method to fix checkstyle

---
+++ InMemoryImpOutputConfig.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class InMemoryImpOutputConfig implements OutputConfig {
+    private final List<String> outputData = new ArrayList<>();
+    @Override
+    public void writeOutput(List<String> outputData) {
+
+    }
+    @Override
+    public String getOutputSource() {
+        return null;
+    }
+    @Override
+    public void setOutputSource(String outputSource) {
+
+    }
+}
