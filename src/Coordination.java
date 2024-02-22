89588d2b7353ff3f491be72351d898581f7d6c28
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-02-21 22:58:12 -0500

    Update Coordination.java

--- src/Coordination.java
+++ src/Coordination.java
@@ -1,6 +1,9 @@
-public class Coordination {
+public class Coordination{
     public void initiate(){
-        DataEngine.readData(UserEngine.getInputSource());
+        //if DataEngine Encountrs Error return
+        if(DataEngine.readData(UserEngine.getInputSource())){
+            return;
+        }
         DataEngine.writeData(UserEngine.getOutputDestination(), FactoringImp.findFactors(DataEngine.getNumbers()));
     }
 }
