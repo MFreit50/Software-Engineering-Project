a0aa43964c2ba4d8c1a5fb54e4f0bb5eef7fad05
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-02-29 16:53:17 -0500

    Update Coordination.java
    fixed if statement to retrieve the boolean value from EngineStatus enum

--- src/Coordination.java
+++ src/Coordination.java
@@ -1,7 +1,8 @@
 public class Coordination{
     public void initiate(){
+        //final DataEngine.EngineStatus status = DataEngine.readData(UserEngine.getInputSource();
         //if DataEngine Encountrs Error return
-        if(DataEngine.readData(UserEngine.getInputSource())){
+        if(DataEngine.readData(UserEngine.getInputSource()).status){
             return;
         }
         DataEngine.writeData(UserEngine.getOutputDestination(), FactoringImp.findFactors(DataEngine.getNumbers()));
