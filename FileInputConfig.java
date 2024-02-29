7ca1b99dc06ce7953763e947382e2cbd19947fce
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-02-29 18:46:51 -0500

    Updated Input Output Config and their impl (#81)

---
+++ FileInputConfig.java
@@ -0,0 +1,28 @@
+import java.util.List;
+
+public class FileInputConfig implements InputConfig{
+
+    private final String filePath;
+
+    public FileInputConfig(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getFileName() {
+        return filePath;
+    }
+    @Override
+    public List<Integer> getInputData() {
+        return null;
+    }
+
+    @Override
+    public String getInputSource() {
+        return null;
+    }
+
+    @Override
+    public void setInputSource(String inputSource) {
+
+    }
+}
