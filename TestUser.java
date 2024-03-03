faeff449a628ef06660807e395e34288c8c0516c
TanvirKhalid <157427555+TanvirKhalid@users.noreply.github.com>
2024-03-03 17:19:48 -0500

    Add files via upload
    Finished TODO's for TestUser

---
+++ TestUser.java
@@ -0,0 +1,27 @@
+import java.io.File;
+import java.io.IOException;
+
+
+public class TestUser {
+	
+	// TODO 3: change the type of this variable to the name you're using for your
+	// User <-> ComputeEngine API; also update the parameter passed to the constructor
+	private final Coordinator coordinator;
+
+	public TestUser(Coordinator coordinator) {
+		this.coordinator = coordinator;
+	}
+
+	public void run(String outputPath) {
+		String delimiter = ";";
+		String inputPath = "test" + File.separatorChar + "testInputFile.test";
+		
+		// TODO 4: Call the appropriate method(s) on the coordinator to get it to 
+		// run the compute job specified by inputPath, outputPath, and delimiter
+		try {
+			coordinator.initiate(new UserRequest(new FileInputConfig(inputPath), new FileOutputConfig(outputPath), String.valueOf(delimiter)));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
