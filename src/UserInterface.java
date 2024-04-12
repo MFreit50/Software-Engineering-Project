07df6d62a6f5e452feae67ae9b246161f05c7213
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-04-12 01:24:38 -0400

    Create UserInterface.java

---
+++ src/UserInterface.java
@@ -0,0 +1,19 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class UserInterface{
+    public static void main(String[] args) throws IOException{
+        inititate();
+    }
+    public static void inititate() throws IOException{
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insert file path");
+        FileInputConfig fileInputPath = new FileInputConfig(sc.nextLine());
+        System.out.println("Insert output path");
+        FileOutputConfig fileOutputPath = new FileOutputConfig(sc.nextLine());
+        UserRequest userRequest = new UserRequest(fileInputPath, fileOutputPath);
+        Coordinator coordinator = new Coordinator();
+        coordinator.initiate(userRequest);
+        sc.close();
+    }
+}
