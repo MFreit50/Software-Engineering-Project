ad69917c9855a2e043e640d310f57401935e4a67
TanvirKhalid <anjumt31@gmail.com>
2024-04-14 16:09:10 -0400

    Updated UserComputeClient to fix method not found error

--- src/usercompute/UserComputeEngineClient.java
+++ src/usercompute/UserComputeEngineClient.java
@@ -3,6 +3,10 @@ package usercompute;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 public class UserComputeEngineClient {
     private final ManagedChannel channel;
     private final UserComputeEngineServiceGrpc.UserComputeEngineServiceBlockingStub blockingStub;
@@ -15,13 +19,17 @@ public class UserComputeEngineClient {
     }
 
     public void shutdown() throws InterruptedException {
-        channel.shutdown().awaitTermination(5, java.util.concurrent.TimeUnit.SECONDS);
+        channel.shutdown().awaitTermination(5, TimeUnit.SECONDS);
     }
 
     public void findFactors(int[] numbers) {
-        // Create request
+
+        List<Integer> numsList = new ArrayList<>(numbers.length);
+        for (int num : numbers) {
+            numsList.add(num);
+        }
         UserComputeEngine.FindFactorsRequest request = UserComputeEngine.FindFactorsRequest.newBuilder()
-                .addAllNumbers(numbers)
+                .addAllNums(numsList)
                 .build();
 
         UserComputeEngine.FindFactorsResponse response = blockingStub.findFactors(request);
