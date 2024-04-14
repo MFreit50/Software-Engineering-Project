276a38cd7101ff55daf745ae9416e4f8f9df75c7
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-04-14 16:14:50 -0400

    Update DataEngineServiceImpl.java

--- src/dataengine/DataEngineServiceImpl.java
+++ src/dataengine/DataEngineServiceImpl.java
@@ -1,31 +1,28 @@
 package dataengine;
 
+import java.util.Arrays;
 import java.util.List;
 
-import dataengine.DataEngineServiceOuterClass;
-import dataengine.DataEngineServiceOuterClass.ReadDataRequest;
-import dataengine.DataEngineServiceOuterClass.ReadDataResponse;
-import dataengine.DataEngineServiceOuterClass.WriteDataRequest;
-import dataengine.DataEngineServiceOuterClass.WriteDataResponse;
+import dataengine.DataEngineService;
+import dataengine.DataResultService.DataResult;
+import dataengine.DataEngineService.ReadDataRequest;
+import dataengine.DataEngineService.ReadDataResponse;
+import dataengine.DataEngineService.WriteDataRequest;
+import dataengine.DataEngineService.WriteDataResponse;
 import io.grpc.stub.StreamObserver;
 import java.io.IOException;
 
 
-import dataengine.DataEngineServiceGrpc.DataEngineServiceImplBase;
+import dataengine.DataEngineGrpc.DataEngineImplBase;
 
-public class DataEngineServiceImpl extends DataEngineServiceImplBase {
-
-    private final DataEngine dataEngine;
-
-    public DataEngineServiceImpl(DataEngine dataEngine) {
-        this.dataEngine = dataEngine;
-    }
+public class DataEngineServiceImpl extends DataEngineImplBase {
 
     @Override
     public void readData(ReadDataRequest request, StreamObserver<ReadDataResponse> responseObserver) {
+        final DataEngine dataEngine;
         ReadDataResponse response;
         try{
-            DataResult result = dataEngine.readData(request.getFileInputPath());
+            dataengine.DataResult result =  dataEngine.readData(request.getFileInputPath());
             response = ReadDataResponse.newBuilder().setDataResult(result).build();
         }catch(Exception e){
             response = ReadDataResponse.newBuilder().setErrorMessage(e.getMessage()).build();
@@ -37,9 +34,11 @@ public class DataEngineServiceImpl extends DataEngineServiceImplBase {
 
     @Override
     public void writeData(WriteDataRequest request, StreamObserver<WriteDataResponse> responseObserver) {
+        final DataEngine dataEngine;
         WriteDataResponse response;
         try{
-            DataResult result = dataEngine.writeData(request.getFileOutputPath(), request.getDataList());
+            dataengine.DataResult result = dataEngine.writeData(request.getFileOutputPath(), request.getDataList());
+            response = WriteDataResponse.newBuilder().setDataResult(result).build();
         }catch(Exception e){
             response = WriteDataResponse.newBuilder().setErrorMessage(e.getMessage()).build();
         }
