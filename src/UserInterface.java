c2290974ac96adc7529ad4ab7b359b9cf3c324b9
MFreit50 <michaelfreitas.mf50@gmail.com>
2024-04-14 23:26:06 -0400

    Update UserInterface.java
    Uses new Proto generated classes

--- src/UserInterface.java
+++ src/UserInterface.java
@@ -1,7 +1,8 @@
-import dataengine.DataEngineAPI;
-
 import java.io.IOException;
 import java.util.Scanner;
+
+import dataengine.DataEngineService.DataEngineResponse;
+
 public class UserInterface{
     public static void main(String[] args) throws IOException{
         inititate();
@@ -9,13 +10,17 @@ public class UserInterface{
     public static void inititate() throws IOException{
         System.out.println("Current working directory: " + System.getProperty("user.dir"));
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Insert file path");
         FileInputConfig fileInputPath = new FileInputConfig(sc.nextLine());
+
         System.out.println("Insert output path");
         FileOutputConfig fileOutputPath = new FileOutputConfig(sc.nextLine());
+
         UserRequest userRequest = new UserRequest(fileInputPath, fileOutputPath);
         Coordinator coordinator = new Coordinator();
-        DataEngineAPI.EngineStatus dataResult = coordinator.initiate(userRequest);
+
+        DataEngineResponse dataResult = coordinator.initiate(userRequest);
         System.out.println(dataResult.getEngineStatus());
 
         sc.close();
